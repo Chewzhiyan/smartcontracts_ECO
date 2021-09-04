86e41b086d52b0dbdbbb6edd0bf0bd1d86c467bc
Eunice <eunicech@hotmail.sg>
2021-09-05 03:21:26 +0800

    Fix Get Balance and Get Token Balance in balance.js

--- balance.js
+++ balance.js
@@ -9,6 +9,8 @@ window.addEventListener('load', function () {
 })
 
 const showAccount = document.querySelector('.showAccount');
+const contractECO = "0x169dc01a0873e5a306f83d6b233ca20136c483df";
+
 
 //retrieve account from metamask 
 async function getAccount() {
@@ -17,21 +19,7 @@ async function getAccount() {
     // gets the correct ETH wallet from metamask
     console.log(account);
     showAccount.innerHTML = account;
-
-    // const balance = await ethereum.request({ method: 'eth_getBalance', params: ["0xF36EAE3B23009fB9D1864B4377605aD2e667EFd4","latest"], "id":1 });
-    const balance = await ethereum.request({ method: 'eth_getBalance', params: [account,"latest"], "id":1 });
-    // const tbalance = await ethereum.request({ method: 'eth_call', params: [{"to":"0x169dc01a0873e5a306f83d6b233ca20136c483df", "data":"0x169dc01a0873e5a306f83d6b233ca20136c483df"},"latest"], "id":1 });
-
-    function roughScale(x, base) {
-    const parsed = parseInt(x, base);
-    if (isNaN(parsed)) { return 0; }
-    return parsed / (10**18);
-    }
-    showBalance.value = roughScale(balance).toFixed(5);
-    
 }
-
-getAccount();
 
 const promisify = (inner) =>
     new Promise((resolve, reject) =>
@@ -46,20 +34,27 @@ const promisify = (inner) =>
 
 async function getBalance() {
     var address, wei, balance
-    address = document.getElementById("showBalance").value;
+    address = showAccount.innerHTML;
     wei = promisify(cb => web3.eth.getBalance(address, cb))
     try {
         balance = web3.fromWei(await wei, 'ether')
-        document.getElementById("output").innerHTML = balance + " ETH";
+        document.getElementById("showBalance").value = balance.toFixed(5) + " ETH";
     } catch (error) {
         document.getElementById("output").innerHTML = error;
     }
 }
+
 async function getERC20Balance() {
     var address, contractAddress, contractABI, tokenContract, decimals, balance, name, symbol, adjustedBalance
-    address = document.getElementById("address").value
-    contractAddress = document.getElementById("contractAddress").value
+    // address = document.getElementById("address").value
+    // contractAddress = document.getElementById("contractAddress").value
+    
+    address = showAccount.innerHTML
+    contractAddress = contractECO
     contractABI = human_standard_token_abi
+    
+    console.log(address)
+    console.log(contractAddress)
 
     tokenContract = web3.eth.contract(contractABI).at(contractAddress)
 
@@ -70,9 +65,11 @@ async function getERC20Balance() {
 
     try {
         adjustedBalance = await balance / Math.pow(10, await decimals)
-        document.getElementById("output2").innerHTML = adjustedBalance;
-        document.getElementById("output2").innerHTML += " " + await symbol + " (" + await name + ")";
+        document.getElementById("showToken").value = adjustedBalance;
+        document.getElementById("showToken").value += " " + await symbol + " (" + await name + ")";
     } catch (error) {
         document.getElementById("output2").innerHTML = error;
     }
 }
+
+getAccount();
